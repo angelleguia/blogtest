664db9e42759a518a3bf01fd677043c403bd8d87
Lange <angelochek9019@gmail.com>
2023-09-24 21:02:16 -0400

    Layout component and Pages component created.

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Footer from "./components/layout/Footer";
+import Header from "./components/layout/Header";
+import Reference from "./components/pages/Reference";
+import Articles from "./components/pages/Articles";
+import Search from "./components/pages/Search";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Reference />
+      <Articles />
+      <Search />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/pages/Reference.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Reference = () => {
+  return <div>Index</div>;
+};
+
+export default Reference;
